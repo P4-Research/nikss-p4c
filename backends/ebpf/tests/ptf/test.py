f7275c1f68f56c5c684a6271acfc288dbc8ec83c
Jan Palimąka <tatry@users.noreply.github.com>
2021-01-27 13:52:00 +0100

    Simple program to test resubmit path

--- backends/ebpf/tests/ptf/test.py
+++ backends/ebpf/tests/ptf/test.py
@@ -8,7 +8,7 @@ import ptf
 import ptf.testutils as testutils
 from ptf.base_tests import BaseTest
 
-# from scapy.all import *
+from scapy.layers.l2 import Ether
 
 # logger = logging.getLogger('eBPFTest')
 # if not len(logger.handlers):
@@ -20,28 +20,33 @@ PORT2 = 2
 ALL_PORTS = [PORT0, PORT1, PORT2]
 
 
-class BngTest(BaseTest):
+class EbpfTest(BaseTest):
 
     def setUp(self):
-        super(BngTest, self).setUp()
+        super(EbpfTest, self).setUp()
         self.dataplane = ptf.dataplane_instance
         self.dataplane.flush()
 
     def tearDown(self):
-        super(BngTest, self).tearDown()
+        super(EbpfTest, self).tearDown()
 
 
-class ResubmitTest(BngTest):
+class ResubmitTest(EbpfTest):
     """
     Test resubmit packet path. eBPF program should do following operation:
-    1. Modify packet header, e.g. destination MAC may be zeroed.
-    2. Resubmit packet.
-    3. In resubmit path packet should not have been modified, only redirect to egress.
-    4. Input packet and output packet should be the same.
+    1. In NORMAL path: In all packet set source MAC to starts with '00:44'.
+        Test if destination MAC address ends with 'FF:F0' - in this case resubmit.
+    2. In RESUBMIT path destination MAC set to zero.
     Open question: how to verify here that the eBPF program did above operations?
     """
 
     def runTest(self):
         pkt = testutils.simple_ip_packet(eth_dst='00:11:22:33:44:55', eth_src='55:44:33:22:11:00')
         testutils.send_packet(self, PORT0, str(pkt))
+        pkt[Ether].src = '00:44:33:22:11:00'
+        testutils.verify_packet_any_port(self, str(pkt), ALL_PORTS)
+
+        pkt = testutils.simple_ip_packet(eth_dst='00:11:22:33:FF:F0', eth_src='55:44:33:22:11:00')
+        testutils.send_packet(self, PORT0, str(pkt))
+        pkt[Ether].dst = '00:00:00:00:00:00'
         testutils.verify_packet_any_port(self, str(pkt), ALL_PORTS)
