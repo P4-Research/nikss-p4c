b99955361b29d0fa8d56adcaab98594a74f53555
Tomasz Osinski <osinstom@gmail.com>
2021-03-03 11:48:22 +0100

    reformat code

--- backends/ebpf/runtime/prectl/clone_session.h
+++ backends/ebpf/runtime/prectl/clone_session.h
@@ -11,11 +11,11 @@ int do_add_member(int argc, char **argv);
 int do_del_member(int argc, char **argv);
 
 static const struct cmd clone_session_cmds[] = {
-        { "create",	        do_create },
-        { "delete",	        do_delete },
-        { "add-member",	    do_add_member },
-        { "del-member",	    do_del_member },
-        { 0 }
+        {"create",     do_create},
+        {"delete",     do_delete},
+        {"add-member", do_add_member},
+        {"del-member", do_del_member},
+        {0}
 };
 
 #endif //__PRECTL_CLONE_SESSION_H
