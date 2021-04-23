ef863e181774dd3a5bca6db0d65abeb110ccc603
Jan Palimąka <tatry@users.noreply.github.com>
2021-04-23 11:21:18 +0200

    Fix test

--- backends/ebpf/psa/ebpfPipeline.h
+++ backends/ebpf/psa/ebpfPipeline.h
@@ -7,6 +7,9 @@
 
 namespace EBPF {
 
+class EBPFControlPSA;
+class EBPFDeparserPSA;
+
 /*
  * EBPFPipeline represents a single eBPF program in the TC/XDP hook.
  * A single pipeline is composed of Parser, Control block and Deparser.
