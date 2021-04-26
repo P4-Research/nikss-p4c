0a40d824d38895df614dff948ccde99cf3980fc9
Mateusz Kossakowski <mateusz.kossakowski.10@gmail.com>
2021-04-26 15:25:53 +0200

    Implement support for Registers. Add default value test + fix.

--- backends/ebpf/tests/ptf/test.py
+++ backends/ebpf/tests/ptf/test.py
@@ -573,14 +573,14 @@ class RegisterActionPSATest(P4EbpfTest):
         pkt = testutils.simple_ip_packet()
 
         self.update_map(name="ingress_tbl_fwd", key="hex 04 00 00 00", value="hex 01 00 00 00 05 00 00 00")
-        testutils.send_packet(self, PORT0, str(pkt))
-        testutils.verify_packet(self, str(pkt), PORT1)  # Checks action run
+        testutils.send_packet(self, PORT0, pkt)
+        testutils.verify_packet(self, pkt, PORT1)  # Checks action run
 
         self.verify_map_entry(name="ingress_reg", key="hex 05 00 00 00", expected_value="05 00 00 00")
 
         # After next action run in register should be stored a new value - 15
-        testutils.send_packet(self, PORT0, str(pkt))
-        testutils.verify_packet(self, str(pkt), PORT1)
+        testutils.send_packet(self, PORT0, pkt)
+        testutils.verify_packet(self, pkt, PORT1)
 
         self.verify_map_entry(name="ingress_reg", key="hex 05 00 00 00", expected_value="0f 00 00 00")
 
@@ -591,27 +591,48 @@ class RegisterActionPSATest(P4EbpfTest):
 
 class RegisterApplyPSATest(P4EbpfTest):
     """
-        Test register used in an apply block.
-        1. Send a packet on PORT0
-        2. P4 application will read a value (undefined means 0 at current implementation),
-        add 5 and write it to the register
-        3. Verify a value stored in register (5)
-        4. Send a packed on PORT0 second time
-        5. P4 application will read a 5, add 10 and write it to the register
-        6. Verify a value stored in register (15)
+    Test register used in an apply block.
+    1. Send a packet on PORT0
+    2. P4 application will read a value (undefined means 0 at current implementation),
+    add 5 and write it to the register
+    3. Verify a value stored in register (5)
+    4. Send a packed on PORT0 second time
+    5. P4 application will read a 5, add 10 and write it to the register
+    6. Verify a value stored in register (15)
     """
     p4_file_path = "samples/p4testdata/register-apply.p4"
 
     def runTest(self):
         pkt = testutils.simple_ip_packet()
 
-        testutils.send_packet(self, PORT0, str(pkt))
+        testutils.send_packet(self, PORT0, pkt)
         self.verify_map_entry(name="ingress_reg", key="hex 05 00 00 00", expected_value="05 00 00 00")
 
         # After next action run in register should be stored a new value - 15
-        testutils.send_packet(self, PORT0, str(pkt))
+        testutils.send_packet(self, PORT0, pkt)
         self.verify_map_entry(name="ingress_reg", key="hex 05 00 00 00", expected_value="0f 00 00 00")
 
     def tearDown(self):
         self.remove_maps(["ingress_tbl_fwd", "ingress_tbl_fwd_defaultAction", "ingress_reg"])
         super(RegisterApplyPSATest, self).tearDown()
+
+
+class RegisterDefaultPSATest(P4EbpfTest):
+    """
+    Test register used with default value.
+    1. Send a packet on PORT0
+    2. P4 application will read a value (6),
+    add 15 and write it to the register
+    3. Verify a value stored in register (15)
+    """
+    p4_file_path = "samples/p4testdata/register-default.p4"
+
+    def runTest(self):
+        pkt = testutils.simple_ip_packet()
+
+        testutils.send_packet(self, PORT0, pkt)
+        self.verify_map_entry(name="ingress_reg", key="hex 05 00 00 00", expected_value="0f 00 00 00")
+
+    def tearDown(self):
+        self.remove_maps(["ingress_tbl_fwd", "ingress_tbl_fwd_defaultAction", "ingress_reg"])
+        super(RegisterDefaultPSATest, self).tearDown()
