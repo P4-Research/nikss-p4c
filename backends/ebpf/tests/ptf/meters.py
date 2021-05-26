29e049c3a43f0b140b65edb20928648b4ed3dd1a
Mateusz Kossakowski <mateusz.kossakowski.10@gmail.com>
2021-05-26 15:24:42 +0200

    T41: Implement support for Meter extern. Some fixes.

--- backends/ebpf/tests/ptf/meters.py
+++ backends/ebpf/tests/ptf/meters.py
@@ -18,12 +18,12 @@ class InDirectMeterPSATest(P4EbpfTest):
     def runTest(self):
         pkt = testutils.simple_ip_packet()
         # cir, pir -> 512 kb/s, cbs, pbs -> 6400 B (512k + 0,1s)/8
-        self.update_map(name="ingress_meter1", key="hex 00 00 00 00",
+        self.update_map(name="ingress_meter1", key="hex 00",
                         value="hex 00 02 00 00 00 02 00 00 00 19 00 00 00 19 00 00 00 00 00 00 00 00 00 00 00 19 00 00 00 19 00 00")
         testutils.send_packet(self, PORT0, pkt)
         testutils.verify_packet(self, pkt, PORT1)
         # Expecting pbs_left, cbs_left 6400 B - 100 B = 6300 B
-        self.verify_map_entry(name="ingress_meter1", key="hex 00 00 00 00",
+        self.verify_map_entry(name="ingress_meter1", key="hex 00",
                               expected_value="hex 00 02 00 00 00 02 00 00 00 19 00 00 00 19 00 00 00 00 00 00 00 00 00 00 9c 18 00 00 9c 18 00 00")
 
     def tearDown(self):
@@ -43,14 +43,14 @@ class InDirectMeterActionPSATest(P4EbpfTest):
         pkt = testutils.simple_ip_packet()
 
         # cir, pir -> 2048 kb/s, cbs, pbs -> 25600 B (2048k + 0,1s)/8
-        self.update_map(name="ingress_meter1", key="hex 00 00 00 00",
+        self.update_map(name="ingress_meter1", key="hex 00",
                         value="hex 00 08 00 00 00 08 00 00 00 64 00 00 00 64 00 00 00 00 00 00 00 00 00 00 00 64 00 00 00 64 00 00")
         self.update_map(name="ingress_tbl_fwd", key="hex 04 00 00 00", value="hex 01 00 00 00 05 00 00 00")
 
         testutils.send_packet(self, PORT0, pkt)
         testutils.verify_packet(self, pkt, PORT1)
         # Expecting pbs_left, cbs_left 25600 B - 100 B = 25500 B
-        self.verify_map_entry(name="ingress_meter1", key="hex 00 00 00 00",
+        self.verify_map_entry(name="ingress_meter1", key="hex 00",
                               expected_value="hex 00 02 00 00 00 02 00 00 00 19 00 00 00 19 00 00 00 00 00 00 00 00 00 00 9c 63 00 00 9c 63 00 00")
 
     def tearDown(self):
