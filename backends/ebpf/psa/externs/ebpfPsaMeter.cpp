6a7b052582926e8cadd501e7bf545ccc6802fbf9
Mateusz Kossakowski <mateusz.kossakowski.10@gmail.com>
2021-06-16 18:00:39 +0200

    Add good meter implementation + update tests

--- backends/ebpf/psa/externs/ebpfPsaMeter.cpp
+++ backends/ebpf/psa/externs/ebpfPsaMeter.cpp
@@ -90,141 +90,115 @@ void EBPFMeterPSA::emitExecute(CodeBuilder* builder, const P4::ExternMethod* met
     }
 
     auto indexArgExpr = method->expr->arguments->at(0)->expression->to<IR::PathExpression>();
-    // TODO packet len at XDP
+    // TODO packet len at XDP and timestamp at XDP
+    auto timeNs = Util::printf_format("%s->tstamp", pipeline->contextVar.c_str());
     if (type == BYTES) {
         auto packetLen = Util::printf_format("%s->len", pipeline->contextVar.c_str());
-        builder->appendFormat("meter_execute_bytes(&%s, &%s, &%s)", instanceName, packetLen,
-                              indexArgExpr->path->name.name);
+        builder->appendFormat("meter_execute_bytes(&%s, &%s, &%s, &%s)", instanceName, packetLen,
+                              indexArgExpr->path->name.name, timeNs);
     } else {
-        builder->appendFormat("meter_execute_packets(&%s, &%s)", instanceName,
-                              indexArgExpr->path->name.name);
+        builder->appendFormat("meter_execute_packets(&%s, &%s, &%s)", instanceName,
+                              indexArgExpr->path->name.name, timeNs);
     }
 }
 
 cstring EBPFMeterPSA::meterExecuteFunc(bool trace) {
     cstring meterExecuteFunc = "static __always_inline\n"
-                               "int enough_tokens(u32 *tokens, u32 *packet_len, u32 *bs, "
-                               "u32 *bs_left, u32 *ir, u64 *delta_t, u32 *factor) {\n"
+                               "enum PSA_MeterColor_t meter_execute(void *map, "
+                               "u32 *packet_len, void *key, u64 *time_ns) {\n"
+                               "    meter_value *value = BPF_MAP_LOOKUP_ELEM(*map, key);\n"
                                "\n"
-                               "    *tokens = *bs_left + (*delta_t * *ir) / *factor;\n"
+                               "    if (value != NULL) {\n"
+                               "        u64 delta_p, delta_c;\n"
+                               "        u64 n_periods_p, n_periods_c, tokens_pbs, tokens_cbs;\n"
+                               "        bpf_spin_lock(&value->lock);\n"
+                               "        delta_p = *time_ns - value->time_p;\n"
+                               "        delta_c = *time_ns - value->time_c;\n"
                                "\n"
-                               "    if (*tokens > *bs) {\n"
-                               "        *tokens = *bs;\n"
-                               "    }\n"
+                               "        n_periods_p = delta_p / value->pir_period;\n"
+                               "        n_periods_c = delta_c / value->cir_period;\n"
                                "\n"
-                               "    if (*packet_len > *tokens) {\n"
-                               "%trace_msg_no_enough_tokens%"
-                               "        return 0; // No\n"
-                               "    }\n"
+                               "        value->time_p += n_periods_p * value->pir_period;\n"
+                               "        value->time_c += n_periods_c * value->cir_period;\n"
                                "\n"
-                               "%trace_msg_enough_tokens%"
-                               "    return 1; // Yes, enough tokens\n"
-                               "}\n"
+                               "        tokens_pbs = value->pbs_left + "
+                               "n_periods_p * value->pir_unit_per_period;\n"
+                               "        if (tokens_pbs > value->pbs) {\n"
+                               "            tokens_pbs = value->pbs;\n"
+                               "        }\n"
+                               "        tokens_cbs = value->cbs_left + "
+                               "n_periods_c * value->cir_unit_per_period;\n"
+                               "        if (tokens_cbs > value->cbs) {\n"
+                               "            tokens_cbs = value->cbs;\n"
+                               "        }\n"
                                "\n"
-                               "static __always_inline\n"
-                               "enum PSA_MeterColor_t meter_execute(meter_value *value, "
-                               "u32 *packet_len, u32 *factor) {\n"
-                               "%trace_msg_meter_execute%"
-                               "    u64 time_ns = bpf_ktime_get_ns();\n"
-                               "    u64 delta_t = time_ns - value->timestamp;\n"
-                               "    u32 tokens_pbs = 0;\n"
-                               "    if (enough_tokens(&tokens_pbs, packet_len, &value->pbs, "
-                               "&value->pbs_left, &value->pir, &delta_t, factor)) {\n"
-                               "        u32 tokens_cbs = 0;\n"
-                               "        if (enough_tokens(&tokens_cbs, packet_len, &value->cbs, "
-                               "&value->cbs_left, &value->cir, &delta_t, factor)) {\n"
-                               "            value->timestamp = value->timestamp + delta_t;\n"
+                               "        if (*packet_len > tokens_pbs) {\n"
+                               "            value->pbs_left = tokens_pbs;\n"
+                               "            value->cbs_left = tokens_cbs;\n"
+                               "            bpf_spin_unlock(&value->lock);\n"
+                                            "%trace_msg_meter_red%"
+                               "            return RED;\n"
+                               "        }\n"
+                               "\n"
+                               "        if (*packet_len > tokens_cbs) {\n"
                                "            value->pbs_left = tokens_pbs - *packet_len;\n"
-                               "            value->cbs_left = tokens_cbs - *packet_len;\n"
-                               "%trace_msg_meter_green%"
-                               "            return GREEN;\n"
-                               "        } else {\n"
-                               "            value->timestamp = value->timestamp + delta_t;\n"
-                               "            value->pbs_left = tokens_pbs - *packet_len;\n"
-                               "%trace_msg_meter_yellow%"
+                               "            value->cbs_left = tokens_cbs;\n"
+                               "            bpf_spin_unlock(&value->lock);\n"
+                                            "%trace_msg_meter_yellow%"
                                "            return YELLOW;\n"
                                "        }\n"
-                               "    } else {\n"
-                               "%trace_msg_meter_red%"
-                               "        return RED;\n"
-                               "    }\n"
-                               "}\n"
                                "\n"
-                               "static __always_inline\n"
-                               "enum PSA_MeterColor_t meter_execute_bytes_value(meter_value *value,"
-                               "u32 *packet_len) {\n"
-                               "    u32 factor = 8000000;\n"
-                               "    if (value != NULL) {\n"
-                               "        return meter_execute(value, packet_len, &factor);\n"
+                               "        value->pbs_left = tokens_pbs - *packet_len;\n"
+                               "        value->cbs_left = tokens_cbs - *packet_len;\n"
+                               "        bpf_spin_unlock(&value->lock);\n"
+                                        "%trace_msg_meter_green%"
+                               "        return GREEN;\n"
                                "    } else {\n"
-                               "%trace_msg_meter_no_value%"
-                               "        return RED;\n"
+                               "        // From P4Runtime spec. No value - return default GREEN.\n"
+                                        "%trace_msg_meter_no_value%"
+                               "        return GREEN;\n"
                                "    }\n"
                                "}\n"
                                "\n"
                                "static __always_inline\n"
                                "enum PSA_MeterColor_t meter_execute_bytes(void *map, "
-                               "u32 *packet_len, void *key) {\n"
-                               "    meter_value *value = BPF_MAP_LOOKUP_ELEM(*map, key);\n"
-                               "    return meter_execute_bytes_value(value, packet_len);\n"
-                               "}\n"
-                               "\n"
-                               "static __always_inline\n"
-                               "enum PSA_MeterColor_t meter_execute_packets_value("
-                               "meter_value *value) {\n"
-                               "    u32 len = 1;\n"
-                               "    u32 factor = 1000000000;\n"
-                               "    if (value != NULL) {\n"
-                               "        return meter_execute(value, &len, &factor);\n"
-                               "    } else {\n"
-                               "%trace_msg_meter_no_value%"
-                               "        return RED;\n"
-                               "    }\n"
+                               "u32 *packet_len, void *key, u64 *time_ns) {\n"
+                                    "%trace_msg_meter_execute_bytes%"
+                               "    return meter_execute(map, packet_len, key, time_ns);\n"
                                "}\n"
                                "\n"
                                "static __always_inline\n"
                                "enum PSA_MeterColor_t meter_execute_packets(void *map, "
-                               "void *key) {\n"
-                               "    meter_value *value = BPF_MAP_LOOKUP_ELEM(*map, key);\n"
-                               "    return meter_execute_packets_value(value);\n"
+                               "void *key, u64 *time_ns) {\n"
+                                    "%trace_msg_meter_execute_packets%"
+                               "    u32 len = 1;\n"
+                               "    return meter_execute(map, &len, key, time_ns);\n"
                                "}";
 
     if (trace) {
         meterExecuteFunc = meterExecuteFunc
-                .replace(cstring("%trace_msg_no_enough_tokens%"),
-                         "        bpf_trace_message(\""
-                         "Meter: No enough tokens\");\n");
-        meterExecuteFunc = meterExecuteFunc
-                .replace(cstring("%trace_msg_enough_tokens%"),
-                         "        bpf_trace_message(\""
-                         "Meter: Enough tokens\");\n");
-        meterExecuteFunc = meterExecuteFunc
-                .replace(cstring("%trace_msg_meter_execute%"),
-                         "    bpf_trace_message(\""
-                         "Meter: execute\");\n");
-        meterExecuteFunc = meterExecuteFunc
                 .replace(cstring("%trace_msg_meter_green%"),
                          "                bpf_trace_message(\""
-                         "Meter: GREEN\");\n");
+                         "Meter: GREEN\n\");\n");
         meterExecuteFunc = meterExecuteFunc
                 .replace(cstring("%trace_msg_meter_yellow%"),
                          "                bpf_trace_message(\""
-                         "Meter: YELLOW\");\n");
+                         "Meter: YELLOW\n\");\n");
         meterExecuteFunc = meterExecuteFunc
                 .replace(cstring("%trace_msg_meter_red%"),
                          "        bpf_trace_message(\""
-                         "Meter: RED\");\n");
+                         "Meter: RED\n\");\n");
         meterExecuteFunc = meterExecuteFunc
                 .replace(cstring("%trace_msg_meter_no_value%"),
-                         "        bpf_trace_message(\""
-                         "Meter: No meter value!\");\n");
+                         "        bpf_trace_message(\"Meter: No meter value! "
+                         "Returning default GREEN\n\");");
+        meterExecuteFunc = meterExecuteFunc
+                .replace(cstring("%trace_msg_meter_execute_bytes%"),
+                         "        bpf_trace_message(\"Meter: execute BYTES\\n\");\n");
+        meterExecuteFunc = meterExecuteFunc
+                .replace(cstring("%trace_msg_meter_execute_packets%"),
+                         "        bpf_trace_message(\"Meter: execute PACKETS\\n\");\n");
     } else {
-        meterExecuteFunc = meterExecuteFunc.replace(cstring("%trace_msg_no_enough_tokens%"),
-                                                    "");
-        meterExecuteFunc = meterExecuteFunc.replace(cstring("%trace_msg_enough_tokens%"),
-                                                    "");
-        meterExecuteFunc = meterExecuteFunc.replace(cstring("%trace_msg_meter_execute%"),
-                                                    "");
         meterExecuteFunc = meterExecuteFunc.replace(cstring("%trace_msg_meter_green%"),
                                                     "");
         meterExecuteFunc = meterExecuteFunc.replace(cstring("%trace_msg_meter_yellow%"),
@@ -232,6 +206,10 @@ cstring EBPFMeterPSA::meterExecuteFunc(bool trace) {
         meterExecuteFunc = meterExecuteFunc.replace(cstring("%trace_msg_meter_red%"),
                                                     "");
         meterExecuteFunc = meterExecuteFunc.replace(cstring("%trace_msg_meter_no_value%"),
+                                                    "");
+        meterExecuteFunc = meterExecuteFunc.replace(cstring("%trace_msg_meter_execute_bytes%"),
+                                                    "");
+        meterExecuteFunc = meterExecuteFunc.replace(cstring("%trace_msg_meter_execute_packets%"),
                                                     "");
     }
 
--- backends/ebpf/tests/ptf/meters.py
+++ backends/ebpf/tests/ptf/meters.py
@@ -6,6 +6,7 @@ PORT1 = 1
 PORT2 = 2
 ALL_PORTS = [PORT0, PORT1, PORT2]
 
+meter_value_mask = 0xff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_00_00_00_00_00_00_00_00_00_00_00_00_00_00_00_00_00_00_00_00_00_00_00_00
 
 class MeterPSATest(P4EbpfTest):
     """
@@ -17,15 +18,38 @@ class MeterPSATest(P4EbpfTest):
 
     def runTest(self):
         pkt = testutils.simple_ip_packet()
-        # cir, pir -> 512 kb/s, cbs, pbs -> 6400 B (512k + 0,1s)/8
+        # cir, pir -> 2 Mb/s, cbs, pbs -> bs (10 ms) -> 2500 B -> 09 C4
+        # period 1M ns -> 0F 42 40 -> 250 B per period -> FA
         self.update_map(name="ingress_meter1", key="hex 00",
-                        value="hex 00 02 00 00 00 02 00 00 00 19 00 00 00 19 00 00 00 00 00 00 00 00 00 00 00 19 00 00 00 19 00 00")
+                        value="hex "
+                              "40 42 0F 00 00 00 00 00 " # pir_period
+                              "FA 00 00 00 00 00 00 00 " # pir_unit_per_period
+                              "40 42 0F 00 00 00 00 00 " # cir_period
+                              "FA 00 00 00 00 00 00 00 " # cir_unit_per_period
+                              "C4 09 00 00 00 00 00 00 " # pbs
+                              "C4 09 00 00 00 00 00 00 " # cbs
+                              "C4 09 00 00 00 00 00 00 " # pbs_left
+                              "C4 09 00 00 00 00 00 00 " # cbs_left
+                              "00 00 00 00 00 00 00 00 " # time_p
+                              "00 00 00 00 00 00 00 00 " # time_c
+                              "00 00 00 00 00 00 00 00") # Spin lock
         testutils.send_packet(self, PORT0, pkt)
         testutils.verify_packet(self, pkt, PORT1)
-        # Expecting pbs_left, cbs_left 6400 B - 100 B = 6300 B
+        # Expecting pbs_left, cbs_left 2500 B - 100 B = 2400 B -> 09 60
         self.verify_map_entry(name="ingress_meter1", key="hex 00",
-                              expected_value="hex 00 02 00 00 00 02 00 00 00 19 00 00 00 19 00 00 00 00 00 00 00 00 00 00 9c 18 00 00 9c 18 00 00",
-                              mask=0xff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_00_00_00_00_00_00_00_00_ff_ff_ff_ff_ff_ff_ff_ff)
+                              expected_value="hex "
+                                             "40 42 0F 00 00 00 00 00 "
+                                             "FA 00 00 00 00 00 00 00 "
+                                             "40 42 0F 00 00 00 00 00 "
+                                             "FA 00 00 00 00 00 00 00 "
+                                             "C4 09 00 00 00 00 00 00 "
+                                             "C4 09 00 00 00 00 00 00 "
+                                             "60 09 00 00 00 00 00 00 " # pbs_left
+                                             "60 09 00 00 00 00 00 00 " # cbs_left
+                                             "00 00 00 00 00 00 00 00 "
+                                             "00 00 00 00 00 00 00 00 "
+                                             "00 00 00 00 00 00 00 00",
+                              mask=meter_value_mask)
 
     def tearDown(self):
         self.remove_maps(["ingress_meter1"])
@@ -43,17 +67,40 @@ class MeterActionPSATest(P4EbpfTest):
     def runTest(self):
         pkt = testutils.simple_ip_packet()
 
-        # cir, pir -> 2048 kb/s, cbs, pbs -> 25600 B (2048k + 0,1s)/8
+        # cir, pir -> 10 Mb/s, cbs, pbs -> bs (10 ms) -> 6250 B -> 18 6A
+        # period 1ms -> 1250 B per period, 1ms -> 1e6 ns -> 0F 42 40, 1250 -> 04 E2
         self.update_map(name="ingress_meter1", key="hex 00",
-                        value="hex 00 08 00 00 00 08 00 00 00 64 00 00 00 64 00 00 00 00 00 00 00 00 00 00 00 64 00 00 00 64 00 00")
+                        value="hex "
+                              "40 42 0F 00 00 00 00 00 " # pir_period
+                              "E2 04 00 00 00 00 00 00 " # pir_unit_per_period
+                              "40 42 0F 00 00 00 00 00 " # cir_period
+                              "E2 04 00 00 00 00 00 00 " # cir_unit_per_period
+                              "6A 18 00 00 00 00 00 00 " # pbs
+                              "6A 18 00 00 00 00 00 00 " # cbs
+                              "6A 18 00 00 00 00 00 00 " # pbs_left
+                              "6A 18 00 00 00 00 00 00 " # cbs_left
+                              "00 00 00 00 00 00 00 00 " # time_p
+                              "00 00 00 00 00 00 00 00 " # time_c
+                              "00 00 00 00 00 00 00 00") # Spin lock
         self.update_map(name="ingress_tbl_fwd", key="hex 04 00 00 00", value="hex 01 00 00 00 05 00 00 00")
 
         testutils.send_packet(self, PORT0, pkt)
         testutils.verify_packet(self, pkt, PORT1)
-        # Expecting pbs_left, cbs_left 25600 B - 100 B = 25500 B
+        # Expecting pbs_left, cbs_left 6250 B - 100 B = 6150 B -> 18 06
         self.verify_map_entry(name="ingress_meter1", key="hex 00",
-                              expected_value="hex 00 08 00 00 00 08 00 00 00 64 00 00 00 64 00 00 00 00 00 00 00 00 00 00 9c 63 00 00 9c 63 00 00",
-                              mask=0xff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_00_00_00_00_00_00_00_00_ff_ff_ff_ff_ff_ff_ff_ff)
+                              expected_value="hex "
+                                             "40 42 0F 00 00 00 00 00 "
+                                             "E2 04 00 00 00 00 00 00 "
+                                             "40 42 0F 00 00 00 00 00 "
+                                             "E2 04 00 00 00 00 00 00 "
+                                             "6A 18 00 00 00 00 00 00 "
+                                             "6A 18 00 00 00 00 00 00 "
+                                             "06 18 00 00 00 00 00 00 " # pbs_left
+                                             "06 18 00 00 00 00 00 00 " # cbs_left
+                                             "00 00 00 00 00 00 00 00 "
+                                             "00 00 00 00 00 00 00 00 "
+                                             "00 00 00 00 00 00 00 00",
+                              mask=meter_value_mask)
 
     def tearDown(self):
         self.remove_maps(["ingress_meter1"])
@@ -70,15 +117,37 @@ class MeterPacketsPSATest(P4EbpfTest):
 
     def runTest(self):
         pkt = testutils.simple_ip_packet()
-        # cir, pir -> 100 packets/s, cbs, pbs -> 10 packets
+        # cir, pir -> 100 packets/s, period 10M -> 98 96 80, bs -> 10 -> 0A
         self.update_map(name="ingress_meter1", key="hex 00",
-                        value="hex 64 00 00 00 64 00 00 00 0a 00 00 00 0a 00 00 00 00 00 00 00 00 00 00 00 0a 00 00 00 0a 00 00 00")
+                        value="hex "
+                              "80 96 98 00 00 00 00 00 " # pir_period
+                              "01 00 00 00 00 00 00 00 " # pir_unit_per_period
+                              "80 96 98 00 00 00 00 00 " # cir_period
+                              "01 00 00 00 00 00 00 00 " # cir_unit_per_period
+                              "0A 00 00 00 00 00 00 00 " # pbs
+                              "0A 00 00 00 00 00 00 00 " # cbs
+                              "0A 00 00 00 00 00 00 00 " # pbs_left
+                              "0A 00 00 00 00 00 00 00 " # cbs_left
+                              "00 00 00 00 00 00 00 00 " # time_p
+                              "00 00 00 00 00 00 00 00 " # time_c
+                              "00 00 00 00 00 00 00 00") # Spin lock
         testutils.send_packet(self, PORT0, pkt)
         testutils.verify_packet(self, pkt, PORT1)
         # Expecting pbs_left, cbs_left 10 - 1 = 9
         self.verify_map_entry(name="ingress_meter1", key="hex 00",
-                              expected_value="hex 64 00 00 00 64 00 00 00 0a 00 00 00 0a 00 00 00 00 00 00 00 00 00 00 00 09 00 00 00 09 00 00 00",
-                              mask=0xff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_ff_00_00_00_00_00_00_00_00_ff_ff_ff_ff_ff_ff_ff_ff)
+                              expected_value="hex "
+                                             "80 96 98 00 00 00 00 00 "
+                                             "01 00 00 00 00 00 00 00 "
+                                             "80 96 98 00 00 00 00 00 "
+                                             "01 00 00 00 00 00 00 00 "
+                                             "0A 00 00 00 00 00 00 00 "
+                                             "0A 00 00 00 00 00 00 00 "
+                                             "09 00 00 00 00 00 00 00 " # pbs_left
+                                             "09 00 00 00 00 00 00 00 " # cbs_left
+                                             "00 00 00 00 00 00 00 00 "
+                                             "00 00 00 00 00 00 00 00 "
+                                             "00 00 00 00 00 00 00 00",
+                              mask=meter_value_mask)
 
     def tearDown(self):
         self.remove_maps(["ingress_meter1"])
