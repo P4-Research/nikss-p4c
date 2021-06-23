3c83de0c662fa1a8c82622ba3f0a6a39aff4c2b5
majchwo <wmajcher97@gmail.com>
2021-06-23 20:05:34 +0200

    Add if statement checking whether defaultAction is noAction

--- backends/ebpf/runtime/kernel.mk
+++ backends/ebpf/runtime/kernel.mk
@@ -21,6 +21,7 @@ P4C=p4c-ebpf
 TARGET=kernel
 # Extra arguments for the compiler
 ARGS=
+
 # If needed, bpf target files can be hardcoded here
 # This can be any file of type ".c", ".bc" or, ".o"
 BPFOBJ=
