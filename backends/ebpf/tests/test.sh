940b1e639082c1d89a3c345030ae7e1800c20653
Tomasz Osinski <tomasz@opennetworking.org>
2021-10-22 21:11:56 +0200

    Disable using hdr2map in TC

--- backends/ebpf/tests/test.sh
+++ backends/ebpf/tests/test.sh
@@ -160,13 +160,10 @@ for xdp_enabled in "${XDP[@]}" ; do
     for hdr2map_enabled in "${HDR2MAP[@]}" ; do
       TEST_PARAMS+=";hdr2Map='$hdr2map_enabled'"
       # FIXME: hdr2map is not working properly for TC, we should fix it in future
-#      if [ "$xdp_enabled" == "False" ] && [ "$hdr2map_enabled" == "True" ]; then
-#        echo "Test skipped because hdr2map doesn't work properly in TC"
-#        continue
-#      elif [ "$xdp_enabled" == "True" ] && [ "$xdp2tc_mode" == "head" ]; then
-#        echo "Test skipped because xdp2tc=head doesn't work properly with XDP"
-#        continue
-#      fi
+      if [ "$xdp_enabled" == "False" ] && [ "$hdr2map_enabled" == "True" ]; then
+        echo "Test skipped because hdr2map doesn't work properly in TC"
+        continue
+      fi
       # Start tests
       ptf \
       --test-dir ptf/ \
