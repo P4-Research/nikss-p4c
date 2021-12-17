62b445da3be2db8ed2b4a0e5b02240082d1335ab
tosinski <tomasz@opennetworking.org>
2021-12-17 11:14:04 +0100

    Remove utils from eBPF backend

--- backends/ebpf/evaluation/scenarios/test/ebpf/benchmark-maps/lpm-map/10-entries.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for prefix in 8 16 24 32
-do
-    for value in {1..3}
-    do
-        bpftool map update name tx_port key $prefix 0 0 0 $value 00 00 00 value 15 00 00 00
-    done
-done
