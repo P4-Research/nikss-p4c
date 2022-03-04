b68336132f0b7fc435d29a0013cbaf2a8568b89e
Jan Palimąka <tatry@users.noreply.github.com>
2022-03-04 10:34:40 +0100

    update psabpf

--- tools/ci-build.sh
+++ tools/ci-build.sh
@@ -79,7 +79,7 @@ function install_ptf_ebpf_test_deps() (
   git clone --recursive https://github.com/P4-Research/psabpf.git /tmp/psabpf
   cd /tmp/psabpf
   # psabpf is under heavy development, later use git tags when it will be ready to use
-  git reset --hard 1588004
+  git reset --hard e418f91
   ./build_libbpf.sh
   mkdir build
   cd build
